f1a4f91b11c7532d362eca2876ddc878c3b3f893
raphael <raphael.mambo@univ-rouen.fr>
2025-09-17 15:57:22 +0200

    debutTP

---
+++ TP9/ClientUDP.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.net.*;
+
+public static void main( String args[] )
+	{
+		InetAddress addr = InetAddress.getLocalHost();
+		System.out.println( "addresse=" + adr.getHostName() );
+
+		DatagramPacket packet = new DatagramPacket(
+	}
